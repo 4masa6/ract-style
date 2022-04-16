b3b8c1cfa50ed2edf916f4c6c4c8492cc167f651
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-16 11:52:15 +0900

    CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 import "./styles.css";
 
 export default function App() {
-  return <InlineStyle />;
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+    </div>
+  );
 }
